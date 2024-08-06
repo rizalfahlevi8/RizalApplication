56cdf3253839476a41f394ed09e1ae8d6a2db282
Rizal Fahlevi <rizalfahlevi810@gmail.com>
2024-08-06 08:51:36 +0700

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.go = New System.Windows.Forms.Button()
+        Me.btnVariabel = New System.Windows.Forms.Button()
+        Me.btnDataTypes = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'go
+        '
+        Me.go.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.go.Location = New System.Drawing.Point(92, 41)
+        Me.go.Name = "go"
+        Me.go.Size = New System.Drawing.Size(273, 105)
+        Me.go.TabIndex = 0
+        Me.go.Text = "Go"
+        Me.go.UseVisualStyleBackColor = False
+        '
+        'btnVariabel
+        '
+        Me.btnVariabel.Location = New System.Drawing.Point(387, 41)
+        Me.btnVariabel.Name = "btnVariabel"
+        Me.btnVariabel.Size = New System.Drawing.Size(279, 105)
+        Me.btnVariabel.TabIndex = 1
+        Me.btnVariabel.Text = "Variabel Demonstration"
+        Me.btnVariabel.UseVisualStyleBackColor = True
+        '
+        'btnDataTypes
+        '
+        Me.btnDataTypes.Location = New System.Drawing.Point(92, 158)
+        Me.btnDataTypes.Name = "btnDataTypes"
+        Me.btnDataTypes.Size = New System.Drawing.Size(272, 95)
+        Me.btnDataTypes.TabIndex = 2
+        Me.btnDataTypes.Text = "Data Types"
+        Me.btnDataTypes.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnDataTypes)
+        Me.Controls.Add(Me.btnVariabel)
+        Me.Controls.Add(Me.go)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents go As Button
+    Friend WithEvents btnVariabel As Button
+    Friend WithEvents btnDataTypes As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles go.Click
+        MsgBox("Hello Word")
+        MsgBox("how are you")
+        MsgBox("I hope you are well")
+
+        MessageBox.Show("This is another message")
+    End Sub
+
+    Private Sub btnVariabel_Click(sender As Object, e As EventArgs) Handles btnVariabel.Click
+        Dim stFirstName As String
+        Dim stLastName As String
+
+        stFirstName = "Kevin"
+        stLastName = "George"
+
+
+        MsgBox("hello welcome " & stFirstName & " " & stLastName & " I hope you are well")
+
+
+    End Sub
+
+    Private Sub btnDataTypes_Click(sender As Object, e As EventArgs) Handles btnDataTypes.Click
+        Dim stMake As String
+        Dim stModel As String
+        Dim iDoor As Integer
+        Dim stColour As String
+        Dim bTaxed As Boolean
+        Dim iEngineSize As Integer
+        Dim decPrice As Decimal
+        Dim dtDateRegistered As Date
+
+        stMake = "Ford"
+        stModel = "Escort"
+        iDoor = 5
+        stColour = "red"
+        bTaxed = True
+        iEngineSize = 1200
+        decPrice = 999.99
+        dtDateRegistered = #11/2/2024#
+
+
+    End Sub
+End Class
