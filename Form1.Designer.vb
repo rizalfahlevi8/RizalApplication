024701bc27710e878367fee711e9256a10444d9e
Rizal Fahlevi <rizalfahlevi810@gmail.com>
2024-08-06 09:05:03 +0700

    tambah button

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -25,6 +25,7 @@ Partial Class Form1
         Me.go = New System.Windows.Forms.Button()
         Me.btnVariabel = New System.Windows.Forms.Button()
         Me.btnDataTypes = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'go
@@ -55,11 +56,21 @@ Partial Class Form1
         Me.btnDataTypes.Text = "Data Types"
         Me.btnDataTypes.UseVisualStyleBackColor = True
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(394, 170)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(271, 82)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.btnDataTypes)
         Me.Controls.Add(Me.btnVariabel)
         Me.Controls.Add(Me.go)
@@ -72,4 +83,5 @@ Partial Class Form1
     Friend WithEvents go As Button
     Friend WithEvents btnVariabel As Button
     Friend WithEvents btnDataTypes As Button
+    Friend WithEvents Button1 As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -45,4 +45,8 @@
 
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
 End Class
